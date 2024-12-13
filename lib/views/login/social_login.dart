4bed1a1684943b1f2d1188a9fe1a6f9a1872e529
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2024-12-14 02:58:08 +0530

    3rd commit

--- lib/views/login/social_login.dart
+++ lib/views/login/social_login.dart
@@ -16,48 +16,155 @@ class _SocialLoginViewState extends State<SocialLoginView> {
     var media = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor: TColor.gray80,
-      body:SafeArea(
+      body: SafeArea(
         child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                   SizedBox(height: media.height*0.03,),
-                    Image.asset(
-                      "assets/img/app_logo.png",
-                      width: media.width*0.5,
-                      //height: media.height,
-                      fit: BoxFit.contain,
-                    ),
-                    Spacer(),
-                    //SizedBox(height: 25,),
-                   
-                    Text(
-                      "or",
-                      style: TextStyle(
-                        color: TColor.white,
-                        fontSize: 14,
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: media.height * 0.03,
+              ),
+              Image.asset(
+                "assets/img/app_logo.png",
+                width: media.width * 0.5,
+                //height: media.height,
+                fit: BoxFit.contain,
+              ),
+              Spacer(),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/img/apple_btn.png"),
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 15,),
-        
-                    Secondarybutton(title: "Sign up with E-mail", onPressed: (){}),
-        
-                    SizedBox(height: 10,),
-        
-                     Text(
-                      "By registering you agree to our Terms of Use. Learn how we collect, use and share your data",
-                      style: TextStyle(
-                        color: TColor.white,
-                        fontSize: 14,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.35),
+                            blurRadius: 8,
+                            offset: Offset(0, 4))
+                      ]),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/img/apple.png",width: 15,height: 15,color: TColor.white,),
+                      SizedBox(width: 8,),
+                      Text(
+                        "Sign up with Apple",
+                        style: TextStyle(
+                            color: TColor.white,
+                            //fontFamily: "Inter",
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600),
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 15,),
-                  ],
+                    ],
+                  ),
                 ),
               ),
+              SizedBox(height: 15,),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/img/google_btn.png"),
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.white.withOpacity(0.20),
+                            blurRadius: 8,
+                            offset: Offset(0, 4))
+                      ]),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/img/google.png",width: 15,height: 15,color: TColor.gray,),
+                      SizedBox(width: 8,),
+                      Text(
+                        "Sign up with Google",
+                        style: TextStyle(
+                            color: TColor.gray,
+                            //fontFamily: "Inter",
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/img/fb_btn.png"),
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.blue.withOpacity(0.35),
+                            blurRadius: 8,
+                            offset: Offset(0, 4))
+                      ]),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/img/fb.png",width: 15,height: 15,),
+                      SizedBox(width: 8,),
+                      Text(
+                        "Sign up with Facebook",
+                        style: TextStyle(
+                            color: TColor.white,
+                            //fontFamily: "Inter",
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "or",
+                style: TextStyle(
+                  color: TColor.white,
+                  fontSize: 14,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Secondarybutton(title: "Sign up with E-mail", onPressed: () {}),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "By registering you agree to our Terms of Use. Learn how we collect, use and share your data",
+                style: TextStyle(
+                  color: TColor.white,
+                  fontSize: 14,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
