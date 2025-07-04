68b068b7a2634a7e8ab246e850a305858da13c6c
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2025-07-05 01:12:19 +0530

    Progress center data

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -70,43 +70,102 @@ class _HomeViewState extends State<HomeView> {
                 children: [
                   Image.asset("assets/img/home_bg.png"),
                   Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Spacer(),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Statusbutton(
-                              title: 'Active subs',
-                              onPressed: () {},
-                              statuscolor: TColor.secondary,
-                              value: '12',
+                      Image.asset(
+                        "assets/img/app_logo.png",
+                        width: media.width * 0.28,
+                        //height: media.height,
+                        fit: BoxFit.contain,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "\$1,235",
+                        style: TextStyle(
+                            color: TColor.white,
+                            //fontFamily: "Inter",
+                            fontSize: 40,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "This month bills",
+                        style: TextStyle(
+                            color: TColor.gray40,
+                            //fontFamily: "Inter",
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              // decorations box
+                              color: TColor.gray60.withOpacity(0.3),
+                              borderRadius: BorderRadius.circular(16),
+                              border: Border.all(
+                                  color: TColor.border.withOpacity(0.15))),
+                          child: Text(
+                            "See your budget",
+                            style: TextStyle(
+                              color: TColor.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
                             ),
                           ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Expanded(
-                            child: Statusbutton(
-                              title: 'Highest subs',
-                              onPressed: () {},
-                              statuscolor: TColor.primary10,
-                              value: '\$19.99',
-                            ),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Expanded(
-                            child: Statusbutton(
-                              title: 'Lowest subs',
-                              onPressed: () {},
-                              statuscolor: TColor.secondaryG,
-                              value: '\$5.99',
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Spacer(),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Statusbutton(
+                                title: 'Active subs',
+                                onPressed: () {},
+                                statuscolor: TColor.secondary,
+                                value: '12',
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Expanded(
+                              child: Statusbutton(
+                                title: 'Highest subs',
+                                onPressed: () {},
+                                statuscolor: TColor.primary10,
+                                value: '\$19.99',
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Expanded(
+                              child: Statusbutton(
+                                title: 'Lowest subs',
+                                onPressed: () {},
+                                statuscolor: TColor.secondaryG,
+                                value: '\$5.99',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
