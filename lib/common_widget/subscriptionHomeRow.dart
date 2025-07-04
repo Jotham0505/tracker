8c07a6deb92efcadacdb5fec94b240b218eb6322
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2025-07-04 23:58:00 +0530

    color

--- lib/common_widget/subscriptionHomeRow.dart
+++ lib/common_widget/subscriptionHomeRow.dart
@@ -14,51 +14,54 @@ class SubscriptionHomeRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPressed,
-      child: Container(
-        height: 64,
-        decoration: BoxDecoration(
-            // decorations box
-            color: TColor.gray60.withOpacity(0.2),
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: TColor.border.withOpacity(0.15))),
-        alignment: Alignment.center,
-        child: Row(
-          children: [
-            SizedBox(
-              width: 8,
-            ),
-            Image.asset(
-              sObj["icon"],
-              width: 55,
-              height: 55,
-            ),
-            SizedBox(
-              width: 8,
-            ),
-            Expanded(
-              child: Text(
-                sObj["name"],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          height: 64,
+          decoration: BoxDecoration(
+              // decorations box
+
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: TColor.border.withOpacity(0.15))),
+          alignment: Alignment.center,
+          child: Row(
+            children: [
+              SizedBox(
+                width: 8,
+              ),
+              Image.asset(
+                sObj["icon"],
+                width: 55,
+                height: 55,
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: Text(
+                  sObj["name"],
+                  style: TextStyle(
+                    color: TColor.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "\$${sObj["price"]}",
                 style: TextStyle(
                   color: TColor.white,
                   fontSize: 14,
                   fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-            SizedBox(
-              width: 8,
-            ),
-            Text(
-              "\$${sObj["price"]}",
-              style: TextStyle(
-                color: TColor.white,
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
